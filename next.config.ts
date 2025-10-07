a65139b3783ecbef2102f86984dda2087b482971
CrispyBacon1999 <bacon.josh09@gmail.com>
2025-10-07 15:03:43 -0400

    Add posthog analytics to better keep track of what people are visiting

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,20 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: "/events/static/:path*",
+        destination: "https://us-assets.i.posthog.com/static/:path*",
+      },
+      {
+        source: "/events/:path*",
+        destination: "https://us.i.posthog.com/:path*",
+      },
+    ];
+  },
+  // This is required to support PostHog trailing slash API requests
+  skipTrailingSlashRedirect: true,
 };
 
 export default nextConfig;
